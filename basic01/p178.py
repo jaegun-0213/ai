5b3548c75477845f222441799df11b8a0868e075
jaegun-0213 <kimdohy126185@gmail.com>
2023-12-19 17:56:54 +0900

    반복문 리스트기초

---
+++ basic01/p178.py
@@ -0,0 +1,77 @@
+# for i in range(10) :
+#   print (i, end=" ")
+# print('-'*30)
+# for i in range(1,11) :
+#   print (i, end=" ")
+# print('-'*30)
+# for i in range(11,0,-1) :
+#   print (i, end=" ")
+
+# for i in  "I lovr you!" :
+#   print(i,end=" ")
+
+# sum = 0
+# j = 1
+# while True :
+#   if sum >= 50 :
+#     break
+#   else :
+#     j+= 1
+#     sum += 1
+# print(j,sum)
+
+# fac = 1
+# for i in range (1, 11) :
+#   fac = fac * i
+# print(fac)
+
+# count = 0
+# for i in range (1,1001) :
+#   if count == 10 :
+#     print()
+#     count = 0
+#   if i % 3 != 0 :
+#     print(i, end=" ")
+#     count += 1
+
+# i = 1
+# count = 0
+# while i <= 1000 :
+#   if count == 10 :
+#     print()
+#     count = 0
+#   if i % 3 != 0 :
+#     print(i, end=" ")
+#     count += 1
+#   i += 1
+
+# con = "y"
+# while con == "y" :
+#   a = int (input("성적을 입력하세요"))
+#   if 90 >= a <= 100 :
+#     result = "수"
+#   elif 80 >= a <=89 :
+#     result = "우"
+#   elif 70 >= a <=79 :
+#     result = "미"
+#   elif 60 >= a <=69 :
+#     result = "양"
+#   elif 0 >= a <=59 :
+#     result = "가"
+# print (f"등급 : {result}")
+# con = input("계속하시겠습니까? (중단 :q, 계속: y)")
+9
+a = int(input("시작 수를 입력해주세요 :"))
+b = int(input("끝수를 입력해주세요 :"))
+x=True
+for i in range (a,b+1) :
+  x = True
+  for j in range(2, a) :
+    if i % j == 0 :
+       x = False
+       break
+
+if x :
+   print(i, end=" ")
+
+print(f"{31}은 소수이다")
---
+++ basic01/p182.py
@@ -0,0 +1,46 @@
+# 리스트 특징
+# 여러 자료들의 목록형태로 관리하는 자료구조
+# 특징 : 순서가 있다, 수정이 가능하다, 요소의 종류가 달라도 된다.
+list1 = [1,1.1,"문자", [1,9],7,8]
+print(list1[1:5:3])
+
+list1 = [3,15,-12.5,"사과","딸기"]
+print(list1)
+print(type(list1))
+
+#리스트를 만드는 방법
+# 방법 1 : 변수=[요소들]
+#방법 2 : 리스트()라는 함수를 사용하기
+list2 = list ( range(1,21,2) )
+print(list2)
+
+list3 = list("abcdef")
+print (list3)
+print(type(list3))
+
+list4 = list()
+print (list4)
+
+coleor = ["빨강", "주황", "노랑", "초록", "파랑", "남색", "보라"]
+print(coleor[0])
+print(coleor[5])
+print(coleor[2:6])
+print(coleor[-3])
+print(coleor[-4: -1])
+print(coleor[0:])
+print(coleor[3:])
+print(coleor[0:3])
+print(coleor[-8 : 3])
+print(coleor[1],coleor[4],coleor[6])
+
+list6 = (coleor[1],coleor[4],coleor[6])
+print(list6)
+print(type(list6))
+
+# 반복문과 리스트
+
+for i in coleor :
+  #print(i[0], end=" ")
+  print("나는 %s을 좋아합니다"%i)
+
+print(len(coleor)) # len(리스트) 리스트 요소의 갯수
